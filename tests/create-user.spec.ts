ac7d362b2123f46c5bdc69d0a22e92a1f345458b
Régi GOUALE <regi.gouale@gmail.com>
2026-07-02 01:33:47 +0200

    Ajout de la configuration Biome, mise à jour des dépendances dans package.json, et amélioration des tests avec des ajustements dans create-user.spec.ts.

--- tests/create-user.spec.ts
+++ tests/create-user.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test("Créer un utilisateur sur TaskFlow", async ({ page }) => {
   await page.goto(`./sign-up`);
